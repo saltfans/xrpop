c77d050ee8104f35c1548e1df5b5fc77aa5a60b2
saltfans <aff.davis@gmail.com>
2024-06-08 22:43:20 +0200

    go

---
+++ app/search/all/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const AllSearchPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/search'); // Redirect to /search
+  }, []); // This effect runs once when the component mounts
+
+  return null; // Render nothing, since the redirect is handled in the useEffect
+};
+
+export default AllSearchPage;
