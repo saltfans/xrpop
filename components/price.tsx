891ab08f17a944556e84fa0aa23d48364c39e43f
saltfans <aff.davis@gmail.com>
2024-06-16 22:46:42 +0200

    update

--- components/price.tsx
+++ components/price.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { CiDiscount1 } from "react-icons/ci";
 
 const Price = ({
   amount,
@@ -15,26 +16,40 @@ const Price = ({
   currencyCodeClassName?: string;
   sale?: boolean;
 } & React.ComponentProps<'p'>) => (
+  <div className={clsx(
+    'text-lg', // Base text-lg class
+    'flex', // Flexbox container to center items
+    'justify-center', // Center horizontally
+    'items-center', // Center vertically
+    'font-semibold',
+    className // Additional className passed from props
+  )}>
   <p suppressHydrationWarning={true} className={className}>
-    {compareAtAmount && (
-      <span className="line-through text-red-500 mr-2">
-        {`${new Intl.NumberFormat(undefined, {
+      <span>
+        {new Intl.NumberFormat(undefined, {
           style: 'currency',
           currency: currencyCode,
           currencyDisplay: 'narrowSymbol'
-        }).format(parseFloat(compareAtAmount))}`}
+        }).format(parseFloat(amount))}
       </span>
-    )}
-    <span>
-      {new Intl.NumberFormat(undefined, {
-        style: 'currency',
-        currency: currencyCode,
-        currencyDisplay: 'narrowSymbol'
-      }).format(parseFloat(amount))}
-    </span>
-    <span className={clsx('ml-1 inline', currencyCodeClassName)}>{`${currencyCode}`}</span>
-    {sale && <span className="text-red-500 ml-1">Sale</span>}
+      <span className={clsx('ml-1 inline', currencyCodeClassName)}>{`${currencyCode}`}</span>
+      
+      {compareAtAmount && (
+        <span className="line-through text-red-800 ml-1">
+          {`${new Intl.NumberFormat(undefined, {
+            style: 'currency',
+            currency: currencyCode,
+            currencyDisplay: 'narrowSymbol'
+          }).format(parseFloat(compareAtAmount))}`}
+        </span>
+      )}
+      {sale && (
+        <span className={clsx('text-white ml-1', currencyCodeClassName)}>
+          <CiDiscount1 className='inline-block font-extrabold align-middle text-3xl' />
+        </span>
+      )}
   </p>
+  </div>
 );
 
 export default Price;
